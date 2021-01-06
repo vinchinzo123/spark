bc7dbc96de0e86e80fdef6c2d0a79f05c5e8c268
Menos  Velasco jr <menozvelasco1@gmail.com>
2021-01-06 10:53:33 -0800

    test git push

--- dates/models.py
+++ dates/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+from preferences.models import Preferences
+
+
+class DatesNightModel(models.Model):
+	users = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users", null=True)
+
+	category = models.ForeignKey(Preferences, on_delete=models.CASCADE)
+
+	def __str__(self):
+		return f'{self.users}'
