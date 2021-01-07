59022598bb5ddf3df7250ce375f7b79578b15078
Menos  Velasco jr <menozvelasco1@gmail.com>
2021-01-06 16:02:48 -0800

    datesNightModel is working

--- dates/models.py
+++ dates/models.py
@@ -1,13 +1,21 @@
 from django.db import models
+from django.utils import timezone
 
 from users.models import User
 from preferences.models import Preferences
 
 
 class DatesNightModel(models.Model):
-	users = models.ForeignKey(User, on_delete=models.CASCADE, related_name="users", null=True)
-
 	category = models.ForeignKey(Preferences, on_delete=models.CASCADE)
 
+	users_one = models.ForeignKey(User, on_delete=models.CASCADE, related_name="one", null=True)
+
+	users_two = models.ForeignKey(User, on_delete=models.CASCADE, related_name="two", null=True)
+
+	location = models.CharField(max_length=100)
+
+	when_date_time = models.DateTimeField(default=timezone.now)
+
+
 	def __str__(self):
-		return f'{self.users}'
+		return f'{self.category}'
