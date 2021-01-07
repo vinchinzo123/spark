4da2405759d9ca166e6aa6fe5ec5d66637bc933d
Menos  Velasco jr <menozvelasco1@gmail.com>
2021-01-07 13:56:11 -0800

    urls path and html template

--- users/views.py
+++ users/views.py
@@ -8,7 +8,20 @@ from users.models import User
 def index(request):
     return render (request, "index.html", {})
 
+def profile_view(request):
+    return render(request, "profile.html",{})
 
+def create_a_date_view(request):
+    return render (request, "create_A_date.html", {})
+
+
+
+def preferences_view(request):
+    return render (request, "preferences.html", {})
+
+
+def pending_dates_view(request):
+    return render (request, "pending_dates.html", {})
 
 
 def login_view(request):
